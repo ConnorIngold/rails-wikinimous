fa4d7a4ed808d38fdd6f70958fd570747fb9a0b0
Connor Ingold <connoringold@gmail.com>
2018-02-21 00:26:25 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+  def index
+    @articles = artcile.all
+  end
+
+  def show
+    @artcile = artcile.find(params[:id])
+  end
+
+  def new
+    @artcile = artcile.new
+  end
+
+  def create
+    @artcile = artcile.create(artcile_params)
+    redirect_to artcile_path(@artcile)
+  end
+
+  def edit
+    @artcile = artcile.find(params[:id])
+  end
+
+  def update
+    @artcile = artcile.find(params[:id])
+    @artcile.update(artcile_params)
+    redirect_to Articles_path
+  end
+
+  def delete
+    @artcile = artcile.find(params[:id])
+    @artcile.destroy
+    redirect_to Articles_path
+  end
+
+  def set_task
+    @artcile
+  end
+
+  private
+  def artcile_params
+     params.require(:artcile).permit(:title, :content, :completed)
+  end
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'faker'
+
+10.times do
+  Article.create(:title => Faker::Name.name, :content => Faker::ElderScrolls.dragon)
+  puts "seeding..."
+end
+puts "seeding complete!"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
